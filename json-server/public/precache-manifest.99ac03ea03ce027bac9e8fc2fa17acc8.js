a5204146b3dbdc2d79cf23b6239392121cb8c812
Abhishek Verma <vermaa_1@gmail.com>
2020-06-05 12:36:55 +0530

    Build Files Added

---
+++ json-server/public/precache-manifest.99ac03ea03ce027bac9e8fc2fa17acc8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17122b15dbf801c58c082efa07072eac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9922b81c07cfcf9c8781",
+    "url": "/static/css/2.7f0a60d8.chunk.css"
+  },
+  {
+    "revision": "b44253bc9a1d0ce4a068",
+    "url": "/static/css/main.17322a8a.chunk.css"
+  },
+  {
+    "revision": "9922b81c07cfcf9c8781",
+    "url": "/static/js/2.e453c6e1.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.e453c6e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b44253bc9a1d0ce4a068",
+    "url": "/static/js/main.143501ec.chunk.js"
+  },
+  {
+    "revision": "a35616d865ebcd47e92d",
+    "url": "/static/js/runtime-main.185a88bb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
